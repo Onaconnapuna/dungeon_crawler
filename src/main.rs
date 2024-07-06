ca3834db64b713dbb97c3b68d469968183e92c0a
Onaconnapuna <cjgermain12@gmail.com>
2024-07-06 11:00:20 -0400

    added player

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,23 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
