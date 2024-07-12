f2c2cdb684ff56397df90009420816460e83d7c7
Onaconnapuna <cjgermain12@gmail.com>
2024-07-12 12:08:19 -0400

    added collision between the player and enemies, enemies

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
@@ -6,6 +7,7 @@ mod player_input;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
