966b19cfdb14de419ce837df75c10bcecef6909a
Onaconnapuna <cjgermain12@gmail.com>
2024-07-12 11:00:08 -0400

    added monster spawner and glyphs

--- src/components.rs
+++ src/components.rs
@@ -1,7 +1,6 @@
 pub use crate::prelude::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-
 pub struct Render {
     pub color: ColorPair,
     pub glyph: FontCharType,
@@ -9,3 +8,6 @@ pub struct Render {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Enemy;
