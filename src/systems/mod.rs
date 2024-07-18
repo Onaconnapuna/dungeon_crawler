02b96a9b1d48cf435c1f74903dd9175aeb616e0c
Onaconnapuna <cjgermain12@gmail.com>
2024-07-17 21:58:36 -0400

    added goblin and orc structs, as well as the beginnings of a hud

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 mod collisions;
 mod end_turn;
 mod entity_render;
+mod hud;
 mod map_render;
 mod movement;
 mod player_input;
@@ -25,6 +26,7 @@ pub fn build_player_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -36,6 +38,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
