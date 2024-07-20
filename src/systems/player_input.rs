0cbc04f31dced85225e2dc2ae08357b24aad5161
Onaconnapuna <cjgermain12@gmail.com>
2024-07-19 22:45:36 -0400

    added stationary healing

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -23,6 +25,7 @@ pub fn player_input(
             .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
             .unwrap();
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -30,6 +33,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -39,6 +43,7 @@ pub fn player_input(
                     ));
                 });
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -46,6 +51,15 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+        }
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
