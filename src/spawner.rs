438fd33b88d46b00dabbee0cc5e396c80f629738
Onaconnapuna <cjgermain12@gmail.com>
2024-08-02 13:09:12 -0400

    implementing dijkstra pathfinding

--- src/spawner.rs
+++ src/spawner.rs
@@ -27,7 +27,7 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             color: ColorPair::new(WHITE, BLACK),
             glyph,
         },
-        ChashingPlayer {},
+        ChasingPlayer {},
         Health {
             current: hp,
             max: hp,
--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -12,4 +12,19 @@ pub fn chasing(#[resource] map: &Map, ecs: &SubWorld, commands: &mut CommandBuff
 
     let player_pos = player.iter(ecs).nth(0).unwrap().0;
     let player_idx = map_idx(player_pos.x, player_pos.y);
+
+    let search_targets = vec![player_idx];
+    let dijkstra_map = DijkstraMap::new(SCREEN_WIDTH, SCREEN_HEIGHT, &search_targets, map, 1024.0);
+
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
+        let idx = map_idx(pos.x, pos.y);
+        if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
+            let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
+            let destination = if distance > 1.2 {
+                map.index_to_point2d(destination)
+            } else {
+                *player_pos
+            };
+        }
+    })
 }
