1087eaf0d16eedef2ada05960cccd117919f4c86
Onaconnapuna <cjgermain12@gmail.com>
2024-08-08 22:20:26 -0400

    wrote game over screen and the implemented a restart to the game

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ mod prelude {
     pub use crate::turn_state::*;
 }
 
+use legion::systems::Resource;
 use prelude::*;
 
 struct State {
@@ -58,7 +59,46 @@ impl State {
             monster_systems: build_monster_scheduler(),
         }
     }
-    fn game_over(&mut self, ctx: &mut BTerm) {}
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "Slain by a monster, your hero's journey has come to a premature end.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The Amulet of Yala remains unclaimed, and your home town is not saved.",
+        );
+        ctx.print_color_centered(
+            8,
+            YELLOW,
+            BLACK,
+            "Don't worry, you can always try again with a new hero.",
+        );
+        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to playe again");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.ecs = World::default();
+            self.resources = Resources::default();
+            let mut rng = RandomNumberGenerator::new();
+            let map_builder = MapBuilder::new(&mut rng);
+            spawn_player(&mut self.ecs, map_builder.player_start);
+            map_builder
+                .rooms
+                .iter()
+                .skip(1)
+                .map(|r| r.center())
+                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+            self.resources.insert(map_builder.map);
+            self.resources.insert(Camera::new(map_builder.player_start));
+            self.resources.insert(TurnState::AwaitingInput);
+        }
+    }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -83,6 +123,7 @@ impl GameState for State {
                 self.monster_systems
                     .execute(&mut self.ecs, &mut self.resources);
             }
+            TurnState::GameOver => self.game_over(ctx),
         }
         render_draw_buffer(ctx).expect("Render Error");
     }
