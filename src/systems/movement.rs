c358e8c480decbacb184f18d8060f0eb81be73b7
Onaconnapuna <cjgermain12@gmail.com>
2024-10-07 21:21:35 -0400

    finished fov for monsters, wont chase player until in fov

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system(for_each)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     want_move: &WantsToMove,
@@ -10,6 +11,14 @@ pub fn movement(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
 ) {
+    if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+        if let Ok(fov) = entry.get_component::<FieldOfView>() {
+            commands.add_component(want_move.entity, fov.clone_dirty());
+        }
+        if entry.get_component::<Player>().is_ok() {
+            camera.on_player_move(want_move.destination);
+        }
+    }
     if map.can_enter_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
         if ecs
