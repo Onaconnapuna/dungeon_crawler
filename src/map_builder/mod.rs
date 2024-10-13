d30ba30e427ec87d0f39b40460f4fa268750baa0
Onaconnapuna <cjgermain12@gmail.com>
2024-10-13 10:11:06 -0400

    added Box for diff map algo traits

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -23,8 +23,13 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = DrunkardsWalkArchitect {};
-        architect.new(rng)
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {}),
+        };
+        let mut mb = architect.new(rng);
+        mb
     }
     fn fill(&mut self, tile: TileType) {
         self.map.tiles.iter_mut().for_each(|t| *t = tile);
