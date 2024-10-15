ce16e9c2864bcb8634658a79bb5dedff5a8ceb97
Onaconnapuna <cjgermain12@gmail.com>
2024-10-15 12:53:36 -0400

    player and monsters can now activate items

--- src/systems/use_items.rs
+++ src/systems/use_items.rs
@@ -10,8 +10,8 @@ pub fn use_items(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource] m
     <(Entity, &ActivateItem)>::query()
         .iter(ecs)
         .for_each(|(entity, activate)| {
+            let item = ecs.entry_ref(activate.item);
             if let Ok(item) = item {
-                let item = ecs.entry_ref(activate.item);
                 if let Ok(healing) = item.get_component::<ProvidesHealing>() {
                     healing_to_apply.push((activate.used_by, healing.amount));
                 }
@@ -22,4 +22,11 @@ pub fn use_items(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource] m
             commands.remove(activate.item);
             commands.remove(*entity);
         });
+    for heal in healing_to_apply.iter() {
+        if let Ok(mut target) = ecs.entry_mut(heal.0) {
+            if let Ok(health) = target.get_component_mut::<Health>() {
+                health.current = i32::min(health.max, health.current + heal.1);
+            }
+        }
+    }
 }
