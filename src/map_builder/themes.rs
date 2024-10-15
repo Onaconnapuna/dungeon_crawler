7b5246bb118f1674e9eae29c60b7b7637fbe3831
Onaconnapuna <cjgermain12@gmail.com>
2024-10-15 12:59:32 -0400

    used wrong resource for dungeon floor - fixed

--- src/map_builder/themes.rs
+++ src/map_builder/themes.rs
@@ -11,7 +11,7 @@ impl DungeonTheme {
 impl MapTheme for DungeonTheme {
     fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
         match tile_type {
-            TileType::Floor => to_cp437('-'),
+            TileType::Floor => to_cp437('.'),
             TileType::Wall => to_cp437('#'),
         }
     }
