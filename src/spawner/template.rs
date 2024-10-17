9547221f134782bab73cf7ba91a42179bd2f951b
Onaconnapuna <cjgermain12@gmail.com>
2024-10-17 13:28:54 -0400

    working on Templates cont. added Serde crate

---
+++ src/spawner/template.rs
@@ -0,0 +1,28 @@
+use crate::prelude::*;
+use legion::systems::CommandBuffer;
+use ron::de::from_reader;
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::fs::File;
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Template {
+    pub entity_type: EntityType,
+    pub levels: HashSet<usize>,
+    pub frequency: i32,
+    pub name: String,
+    pub glyph: char,
+    pub provides: Option<Vec<(String, i32)>>,
+    pub hp: Option<i32>,
+}
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+pub enum EntityType {
+    Enemy,
+    Item,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Templates {
+    pub entities: Vec<Template>,
+}
