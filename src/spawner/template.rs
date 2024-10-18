52a06655d4bb5ff85a8569b895049a76adfc6cf2
Onaconnapuna <cjgermain12@gmail.com>
2024-10-17 21:20:56 -0400

    consolidating spawning functions for all entities

--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -26,3 +26,75 @@ pub enum EntityType {
 pub struct Templates {
     pub entities: Vec<Template>,
 }
+
+impl Templates {
+    pub fn load() -> Self {
+        let file = File::open("resources/template.ron").expect("Failed opening file");
+        from_reader(file).expect("Unable to load template")
+    }
+    pub fn spawn_entities(
+        &self,
+        ecs: &mut World,
+        rng: &mut RandomNumberGenerator,
+        level: usize,
+        spawn_points: &[Point],
+    ) {
+        let mut available_entities = Vec::new();
+        self.entities
+            .iter()
+            .filter(|e| e.levels.contains(&level))
+            .for_each(|t| {
+                for _ in 0..t.frequency {
+                    available_entities.push(t);
+                }
+            });
+        let mut commands = CommandBuffer::new(ecs);
+        spawn_points.iter().for_each(|pt| {
+            if let Some(entity) = rng.random_slice_entry(&available_entities) {
+                self.spawn_entity(pt, entity, &mut commands);
+            }
+        });
+        commands.flush(ecs);
+    }
+    pub fn spawn_entity(
+        &self,
+        pt: &Point,
+        template: &Template,
+        commands: &mut legion::systems::CommandBuffer,
+    ) {
+        let entity = commands.push((
+            pt.clone(),
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437(template.glyph),
+            },
+            Name(template.name.clone()),
+        ));
+        match template.entity_type {
+            EntityType::Item => commands.add_component(entity, Item {}),
+            EntityType::Enemy => {
+                commands.add_component(entity, Enemy {});
+                commands.add_component(entity, FieldOfView::new(6));
+                commands.add_component(entity, ChasingPlayer {});
+                commands.add_component(
+                    entity,
+                    Health {
+                        current: template.hp.unwrap(),
+                        max: template.hp.unwrap(),
+                    },
+                );
+            }
+        }
+        if let Some(effects) = &template.provides {
+            effects
+                .iter()
+                .for_each(|(provides, n)| match provides.as_str() {
+                    "Healing" => commands.add_component(entity, ProvidesHealing { amount: *n }),
+                    "MagicMap" => commands.add_component(entity, ProvidesDungeonMap {}),
+                    _ => {
+                        println!("Warning: we don't know how to provide {}", provides);
+                    }
+                });
+        }
+    }
+}
